6eab37de595d85ad6ba3ad7e72dafb6e7b4ec1c0
Jahongir <jakhongirt@gmail.com>
2020-03-28 19:15:05 +0500

    Clean Up 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './virus.png';
+import ReactGA from 'react-ga';
 import './App.scss';
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-162098697-1');
+  ReactGA.pageview('/homepage');
+}
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>More Info abt the Virus Coming...</p>
         <a
           className='App-link'
           href='https://reactjs.org'
